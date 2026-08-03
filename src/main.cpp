be8f98e8b791d454ab7c9d89a279f01ccc234695
Carl Schwan <carl@carlschwan.eu>
2026-08-04 01:23:15 +0200

    Use KAboutData::fromAppStreamId
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 #endif
 #include <KLocalizedQmlContext>
 #include <QCommandLineParser>
+#include <QDebug>
 #include <QDir>
 #include <QIcon>
 #include <QQmlApplicationEngine>
@@ -26,7 +27,8 @@
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QStandardPaths>
-#include <QUrl>
+
+using namespace Qt::StringLiterals;
 
 const QString DRIVER(QStringLiteral("QSQLITE"));
 
@@ -39,11 +41,8 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 #endif
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("kontrast"));
 
-    KAboutData aboutData(QStringLiteral("kontrast"),
-                         i18nc("@title", "Kontrast"),
-                         QStringLiteral(KONTRAST_VERSION_STRING),
-                         i18nc("@title", "A contrast checker application"),
-                         KAboutLicense::GPL_V3);
+    auto aboutData = KAboutData::fromAppStreamId(u"org.kde.kontrast"_s);
+    aboutData.setVersion(KONTRAST_VERSION_STRING);
 
     aboutData.addAuthor(i18nc("@info:credit", "Carl Schwan"),
                         i18nc("@info:credit", "Maintainer and creator"),
